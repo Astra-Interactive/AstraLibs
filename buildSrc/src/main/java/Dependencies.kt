1b4421436a596b76e5d4ba064dbbe0794d3567c4
makeevrserg <makeevrserg@gmail.com>
2022-10-19 11:55:34 +0300

    version increased

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 
 object Dependencies {
-    const val version = "2.0.1"
+    const val version = "2.0.2"
     const val group = "ru.astrainteractive.astralibs"
 
     object Kotlin {
