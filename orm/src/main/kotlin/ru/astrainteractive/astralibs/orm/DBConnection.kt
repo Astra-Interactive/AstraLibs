e277facd987b549dffce4b25ad71e8bf1b0674fe
makeevrserg <makeevrserg@gmail.com>
2023-02-18 10:10:07 +0300

    feat: added sessionVariables into DBConnection

--- orm/src/main/kotlin/ru/astrainteractive/astralibs/orm/DBConnection.kt
+++ orm/src/main/kotlin/ru/astrainteractive/astralibs/orm/DBConnection.kt
@@ -2,19 +2,35 @@ package ru.astrainteractive.astralibs.orm
 
 sealed class DBConnection(val driver: String) {
     abstract val url: String
-    class SQLite(val dbName: String) : DBConnection("org.sqlite.JDBC", ){
+
+    class SQLite(val dbName: String) : DBConnection("org.sqlite.JDBC") {
         override val url: String
             get() = "jdbc:sqlite:$dbName"
     }
+
+    /**
+     * @property database database name
+     * @property ip ip address of your MySql server
+     * @property port port of your MySql server
+     * @property username username
+     * @property password password
+     * @property sessionVariables - session variables of MySql connection. Example: sql_mode=''
+     */
     class MySQL(
         val database: String,
         private val ip: String,
         private val port: Int,
         val username: String,
-        val password: String
-    ): DBConnection("com.mysql.cj.jdbc.Driver"){
+        val password: String,
+        vararg val sessionVariables: String
+    ) : DBConnection("com.mysql.cj.jdbc.Driver") {
         override val url: String
-            get() = "jdbc:mysql://$ip:$port/$database?sessionVariables=sql_mode=\'\'"
+            get() {
+                val sv = if (sessionVariables.isEmpty()) ""
+                else sessionVariables.joinToString("&", "?sessionVariables=") { it }
+
+                return "jdbc:mysql://$ip:$port/$database$sv"
+            }
 
     }
 }
