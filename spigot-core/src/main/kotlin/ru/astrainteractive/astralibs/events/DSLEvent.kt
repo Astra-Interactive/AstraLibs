61a9d052eaf60d39151dc8960a2e0c713ad1992a
makeevrserg <makeevrserg@gmail.com>
2023-04-23 10:59:23 +0300

    changed DSLEvent to invoke operator

--- spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/events/DSLEvent.kt
+++ spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/events/DSLEvent.kt
@@ -9,26 +9,23 @@ import org.bukkit.plugin.java.JavaPlugin
  * With this object you will no longer need to create interfaces for your bukkit event listeners
  */
 object DSLEvent {
-    inline fun <reified T : Event> event(
+    inline operator fun <reified T : Event> invoke(
         eventController: EventListener = GlobalEventListener,
         eventPriority: EventPriority = EventPriority.NORMAL,
         plugin: JavaPlugin = AstraLibs.instance,
         crossinline block: (T) -> Unit
-    ) = event(T::class.java, eventController, eventPriority, plugin) {
+    ) = DSLEvent(T::class.java, eventController, eventPriority, plugin) {
         (it as? T)?.let(block)
     }
 
-    fun <T : Event> event(
+    operator fun <T : Event> invoke(
         clazz: Class<T>,
         eventController: EventListener,
         eventPriority: EventPriority,
         plugin: JavaPlugin,
         block: (Event) -> Unit
     ) = plugin.server.pluginManager.registerEvent(
-        clazz, eventController, eventPriority,
-        { _, event ->
-            block(event)
-        }, plugin
+        clazz, eventController, eventPriority, { _, event -> block.invoke(event) }, plugin
     )
 
 
