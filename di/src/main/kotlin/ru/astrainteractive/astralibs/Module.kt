17f097e8ad044dd41dd15e04d82aebbfe5ad08b3
Roman Makeev <makeevrserg@gmail.com>
2023-04-30 18:52:25 +0300

    - Added readProperty for Module

--- di/src/main/kotlin/ru/astrainteractive/astralibs/Module.kt
+++ di/src/main/kotlin/ru/astrainteractive/astralibs/Module.kt
@@ -1,7 +1,11 @@
 package ru.astrainteractive.astralibs
 
+import kotlin.reflect.KProperty
+
 /**
  * [Module] interface is a definition for module package, which will contains
  * other dependencies or even submodules which are also will be [Module]
  */
 interface Module
+
+inline operator fun <reified T : Module, K> T.getValue(t: K?, property: KProperty<*>): T = this
