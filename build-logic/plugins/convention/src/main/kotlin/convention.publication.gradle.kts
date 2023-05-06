ffa42f1df951cd6436b4804ce84a9485e9695234
Roman Makeev <makeevrserg@gmail.com>
2023-05-06 10:32:51 +0300

    - Added auto deploy/tests/detekt

--- build-logic/plugins/convention/src/main/kotlin/convention.publication.gradle.kts
+++ build-logic/plugins/convention/src/main/kotlin/convention.publication.gradle.kts
@@ -1,38 +1,38 @@
+@file:Suppress("VariableNaming")
+
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
-import java.util.Properties
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.registering
+import org.gradle.kotlin.dsl.signing
+import org.gradle.kotlin.dsl.withType
+import java.util.*
 
 plugins {
     `maven-publish`
     signing
-    java
-    `java-library`
 }
 
 // Grabbing secrets from local.properties file or from environment variables, which could be used on CI
-val secretPropsFile = project.rootProject.file(".gradle/gradle.properties")
-val properties = Properties().apply { load(secretPropsFile.reader()) }
-val signingKeyId = properties.getProperty("signing.keyId")
-val signingPassword = properties.getProperty("signing.password")
-val signingSecretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
-val ossrhUsername = properties.getProperty("ossrhUsername")
-val ossrhPassword = properties.getProperty("ossrhPassword")
+val properties = Properties().apply {
+    val secretPropsFile = project.rootProject.file("local.properties")
+    if (secretPropsFile.exists()) {
+        load(secretPropsFile.reader())
+    }
+}
 
-ext["signing.keyId"] = signingKeyId
-ext["signing.password"] = signingPassword
-ext["signing.secretKeyRingFile"] = signingSecretKeyRingFile
-ext["ossrhUsername"] = ossrhUsername
-ext["ossrhPassword"] = ossrhPassword
+val SIGNING_KEY_ID = System.getenv("SIGNING_KEY_ID") ?: properties.getProperty("signing.keyId")
+
+val SIGNING_PASSWORD = System.getenv("SIGNING_PASSWORD") ?: properties.getProperty("signing.password")
+
+val OSSRH_USERNAME = System.getenv("OSSRH_USERNAME") ?: properties.getProperty("ossrhUsername")
+
+val OSSRH_PASSWORD = System.getenv("OSSRH_PASSWORD") ?: properties.getProperty("ossrhPassword")
+
+val SIGNING_KEY = System.getenv("SIGNING_KEY") ?: properties.getProperty("signing.base64")
 
 val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
-}
-artifacts {
-    archives(javadocJar)
-}
-java {
-    withSourcesJar()
-    withJavadocJar()
 }
 
 publishing {
@@ -41,33 +41,21 @@ publishing {
             name = "sonatype"
             setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
-
-        maven {
-            name = "sonatype"
-            setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            credentials {
-                username = ossrhUsername
-                password = ossrhPassword
+                username = OSSRH_USERNAME
+                password = OSSRH_PASSWORD
             }
         }
     }
 
-    publications.create<MavenPublication>("default") {
+    publications.withType<MavenPublication> {
 
         artifact(javadocJar.get())
-        artifact(tasks["sourcesJar"])
 
-        from(components["kotlin"])
         pom {
-            artifactId = project.name
-            groupId = libs.versions.plugin.group.get()
-            version = libs.versions.plugin.version.get()
             name.set(project.name)
-            description.set("Spigot core library written in kotlin")
+            group = libs.versions.plugin.group.get()
+            version = libs.versions.plugin.version.get()
+            description.set(libs.versions.plugin.description.get())
             url.set("https://github.com/Astra-Interactive/AstraLibs")
 
             licenses {
@@ -92,6 +80,20 @@ publishing {
     }
 }
 
+tasks.create("Base64FromGPG") {
+    val SIGNING_SECRET_KEY_RING_FILE = properties.getProperty("signing.secretKeyRingFile") ?: return@create
+    val bytes = File(SIGNING_SECRET_KEY_RING_FILE).readBytes()
+
+    val SIGNING_KEY = Base64.getEncoder().encodeToString(bytes)
+    File("./key.txt").apply {
+        if (!exists()) {
+            createNewFile()
+            writeText(SIGNING_KEY)
+        }
+    }
+}
+
 signing {
+    useInMemoryPgpKeys(SIGNING_KEY_ID, SIGNING_KEY, SIGNING_PASSWORD)
     sign(publishing.publications)
 }
