4f8e4f6cab1f06dcbb8df096005d7f5a2481c5f7
Roman Makeev <makeevrserg@gmail.com>
2023-07-11 18:52:28 +0300

    feat: updated gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 
 buildscript {
     dependencies {
-        classpath("ru.astrainteractive.gradleplugin:convention:0.0.9")
-        classpath("ru.astrainteractive.gradleplugin:minecraft:0.0.9")
+        classpath("ru.astrainteractive.gradleplugin:convention:0.0.10")
+        classpath("ru.astrainteractive.gradleplugin:minecraft:0.0.10")
     }
 }
 
@@ -15,13 +15,14 @@ plugins {
 
 apply(plugin = "ru.astrainteractive.gradleplugin.dokka.root")
 apply(plugin = "ru.astrainteractive.gradleplugin.detekt")
+apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
 
 subprojects.forEach {
     it.apply(plugin = "ru.astrainteractive.gradleplugin.dokka.module")
+    it.apply(plugin = "ru.astrainteractive.gradleplugin.stub.javadoc")
     if (it.name != "bukkit") {
         it.apply(plugin = "ru.astrainteractive.gradleplugin.publication")
     }
-    it.apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
     it.plugins.withId("org.jetbrains.kotlin.jvm") {
         it.apply(plugin = "ru.astrainteractive.gradleplugin.java.core")
     }
