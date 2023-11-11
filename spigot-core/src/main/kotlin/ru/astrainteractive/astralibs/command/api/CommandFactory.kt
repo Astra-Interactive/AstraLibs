3c836fe96c6c36f1ebbef77fd0ece112481a0077
Roman Makeev <makeevrserg@gmail.com>
2023-11-11 10:42:50 +0300

    fox: command factory

--- spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/command/api/CommandFactory.kt
+++ spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/command/api/CommandFactory.kt
@@ -1,14 +1,11 @@
 package ru.astrainteractive.astralibs.command.api
 
-import org.bukkit.plugin.java.JavaPlugin
-
 /**
  * [CommandFactory] will create default [Command] instance. Use with delegates
  */
 interface CommandFactory {
     @Suppress("LongParameterList")
     fun <R : Any, I : Any> create(
-        plugin: JavaPlugin,
         alias: String,
         commandParser: CommandParser<R>,
         commandExecutor: CommandExecutor<I>,
