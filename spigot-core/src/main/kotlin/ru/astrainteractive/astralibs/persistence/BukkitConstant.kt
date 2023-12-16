385709a8cafc58f89ddd72ecc8820054433dddc3
Roman Makeev <makeevrserg@gmail.com>
2023-12-16 10:39:15 +0300

    improve BukkitConstant

--- spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/persistence/BukkitConstant.kt
+++ spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/persistence/BukkitConstant.kt
@@ -4,18 +4,28 @@ import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.Plugin
 
-class BukkitConstant<T, Z>(
-    val plugin: Plugin,
-    val value: NamespacedKey,
+interface BukkitConstant<T, Z> {
+    val value: NamespacedKey
     val dataType: PersistentDataType<T, Z>
-) {
-    constructor(
-        plugin: Plugin,
-        key: String,
-        dataType: PersistentDataType<T, Z>
-    ) : this(
-        plugin = plugin,
+
+    class Default<T, Z>(
+        override val value: NamespacedKey,
+        override val dataType: PersistentDataType<T, Z>
+    ) : BukkitConstant<T, Z>
+}
+
+@Suppress("FunctionNaming")
+fun <T, Z> BukkitConstant(plugin: Plugin, key: String, dataType: PersistentDataType<T, Z>): BukkitConstant<T, Z> {
+    return BukkitConstant.Default<T, Z>(
         value = NamespacedKey(plugin, key),
+        dataType = dataType
+    )
+}
+
+@Suppress("FunctionNaming")
+fun <T, Z> BukkitConstant(namespace: String, key: String, dataType: PersistentDataType<T, Z>): BukkitConstant<T, Z> {
+    return BukkitConstant.Default<T, Z>(
+        value = NamespacedKey(namespace, key),
         dataType = dataType
     )
 }
