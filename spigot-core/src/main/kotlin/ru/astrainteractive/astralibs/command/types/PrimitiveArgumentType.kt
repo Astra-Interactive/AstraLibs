6f4dc77a4c87114ba1c301865bb2cb29b6840442
Roman Makeev <makeevrserg@gmail.com>
2024-01-13 15:19:14 +0300

    add argument type default parameter ext

--- spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/command/types/PrimitiveArgumentType.kt
+++ spigot-core/src/main/kotlin/ru/astrainteractive/astralibs/command/types/PrimitiveArgumentType.kt
@@ -1,27 +1,27 @@
 package ru.astrainteractive.astralibs.command.types
 
-sealed interface PrimitiveArgumentType<T> : ArgumentType<T> {
-    data object Integer : PrimitiveArgumentType<kotlin.Int> {
-        override fun transform(value: kotlin.String): kotlin.Int {
-            return value.toInt()
+interface PrimitiveArgumentType<T> : ArgumentType<T> {
+    data object Int : PrimitiveArgumentType<kotlin.Int?> {
+        override fun transform(value: kotlin.String?): kotlin.Int? {
+            return value?.toIntOrNull()
         }
     }
 
-    data object String : PrimitiveArgumentType<kotlin.String> {
-        override fun transform(value: kotlin.String): kotlin.String {
+    data object String : PrimitiveArgumentType<kotlin.String?> {
+        override fun transform(value: kotlin.String?): kotlin.String? {
             return value
         }
     }
 
-    data object Double : PrimitiveArgumentType<kotlin.Double> {
-        override fun transform(value: kotlin.String): kotlin.Double {
-            return value.toDouble()
+    data object Double : PrimitiveArgumentType<kotlin.Double?> {
+        override fun transform(value: kotlin.String?): kotlin.Double? {
+            return value?.toDouble()
         }
     }
 
-    data object Boolean : PrimitiveArgumentType<kotlin.Boolean> {
-        override fun transform(value: kotlin.String): kotlin.Boolean {
-            return value.toBooleanStrict()
+    data object Boolean : PrimitiveArgumentType<kotlin.Boolean?> {
+        override fun transform(value: kotlin.String?): kotlin.Boolean? {
+            return value?.toBooleanStrict()
         }
     }
 }
