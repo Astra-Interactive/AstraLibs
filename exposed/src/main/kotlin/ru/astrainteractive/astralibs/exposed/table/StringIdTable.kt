792440b85d1f4d00293304dfee4739d386b5fa74
Roman Makeev <makeevrserg@gmail.com>
2024-09-11 09:46:38 +0300

    fix internal StringIdTable

--- exposed/src/main/kotlin/ru/astrainteractive/astralibs/exposed/table/StringIdTable.kt
+++ exposed/src/main/kotlin/ru/astrainteractive/astralibs/exposed/table/StringIdTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
 
-internal open class StringIdTable(name: String = "", columnName: String = "id") : IdTable<String>(name) {
+open class StringIdTable(name: String = "", columnName: String = "id") : IdTable<String>(name) {
     final override val id: Column<EntityID<String>> = text(columnName).entityId()
     final override val primaryKey = PrimaryKey(id)
 }
