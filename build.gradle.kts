e057aa1710d73ffb8b23f9a1b1c0c39238754007
Roman Makeev <makeevrserg@gmail.com>
2024-11-29 22:35:34 +0300

    test new publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,20 +8,15 @@ plugins {
     alias(libs.plugins.klibs.gradle.dokka.root) apply false
     alias(libs.plugins.klibs.gradle.dokka.module) apply false
     alias(libs.plugins.klibs.gradle.java.core) apply false
-    alias(libs.plugins.klibs.gradle.stub.javadoc) apply false
     alias(libs.plugins.klibs.gradle.publication) apply false
     alias(libs.plugins.klibs.gradle.rootinfo) apply false
-    // klibs - minecraft
-    alias(libs.plugins.klibs.gradle.minecraft.empty) apply false
-    alias(libs.plugins.klibs.gradle.minecraft.multiplatform) apply false
 }
 
-apply(plugin = "ru.astrainteractive.gradleplugin.dokka.root")
 apply(plugin = "ru.astrainteractive.gradleplugin.detekt")
 apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
 
 subprojects.forEach {
-    it.apply(plugin = "ru.astrainteractive.gradleplugin.dokka.module")
+    it.apply(plugin = "ru.astrainteractive.gradleplugin.root.info")
     if (it.name != "bukkit" && it.name != "benchmarks") {
         it.apply(plugin = "ru.astrainteractive.gradleplugin.publication")
     }
