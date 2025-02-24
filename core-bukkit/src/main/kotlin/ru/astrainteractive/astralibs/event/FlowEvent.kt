580c34991273165ca451e4e729b9058ab91a6512
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2025-02-24 22:43:13 +0300

    feat: add flow event (#83)

---
+++ core-bukkit/src/main/kotlin/ru/astrainteractive/astralibs/event/FlowEvent.kt
@@ -0,0 +1,42 @@
+package ru.astrainteractive.astralibs.event
+
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.runBlocking
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import org.bukkit.plugin.EventExecutor
+import org.bukkit.plugin.Plugin
+
+inline fun <reified T : Event> flowEvent(
+    plugin: Plugin,
+    eventPriority: EventPriority = EventPriority.NORMAL
+) = flowEvent(
+    plugin = plugin,
+    clazz = T::class.java,
+    eventPriority = eventPriority
+)
+
+fun <T : Event> flowEvent(
+    plugin: Plugin,
+    clazz: Class<T>,
+    eventPriority: EventPriority = EventPriority.NORMAL
+): Flow<Event> = callbackFlow {
+    val eventListener = EventListener.Default()
+    val eventExecutor = EventExecutor { _, event ->
+        if (event::class.java != clazz) return@EventExecutor
+        runBlocking { trySendBlocking(event) }
+    }
+    plugin.server.pluginManager.registerEvent(
+        clazz,
+        eventListener,
+        eventPriority,
+        eventExecutor,
+        plugin
+    )
+    awaitClose {
+        eventListener.onDisable()
+    }
+}
---
+++ core-bukkit/src/main/kotlin/ru/astrainteractive/astralibs/event/InlineEvent.kt
@@ -0,0 +1,28 @@
+package ru.astrainteractive.astralibs.event
+
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import org.bukkit.plugin.Plugin
+
+inline fun <reified T : Event> inlineEvent(
+    eventListener: EventListener,
+    plugin: Plugin,
+    eventPriority: EventPriority = EventPriority.NORMAL,
+    crossinline block: (T) -> Unit
+) = inlineEvent(T::class.java, eventListener, plugin, eventPriority) { event ->
+    (event as? T)?.let(block)
+}
+
+fun <T : Event> inlineEvent(
+    clazz: Class<T>,
+    eventListener: EventListener,
+    plugin: Plugin,
+    eventPriority: EventPriority,
+    block: (Event) -> Unit
+) = plugin.server.pluginManager.registerEvent(
+    clazz,
+    eventListener,
+    eventPriority,
+    { _, event -> block.invoke(event) },
+    plugin
+)
