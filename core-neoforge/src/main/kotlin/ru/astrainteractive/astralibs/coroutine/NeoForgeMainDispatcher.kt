04939778ecbf27eece95e3c3fd19ac53f1caaaa6
Roman Makeev <makeevrserg@gmail.com>
2025-12-02 15:00:21 +0300

    fix dispatcher

--- core-neoforge/src/main/kotlin/ru/astrainteractive/astralibs/coroutine/NeoForgeMainDispatcher.kt
+++ core-neoforge/src/main/kotlin/ru/astrainteractive/astralibs/coroutine/NeoForgeMainDispatcher.kt
@@ -1,20 +1,20 @@
 package ru.astrainteractive.astralibs.coroutine
 
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.MainCoroutineDispatcher
 import net.minecraft.client.Minecraft
-import net.minecraft.server.TickTask
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.fml.loading.FMLEnvironment
 import ru.astrainteractive.astralibs.server.util.NeoForgeUtil
 import java.util.concurrent.Executor
 import kotlin.coroutines.CoroutineContext
 
-class NeoForgeMainDispatcher : CoroutineDispatcher() {
+class NeoForgeMainDispatcher2 : MainCoroutineDispatcher() {
+    override val immediate: MainCoroutineDispatcher
+        get() = this
     private val executor = when (FMLEnvironment.dist) {
         Dist.CLIENT -> Minecraft.getInstance()
         Dist.DEDICATED_SERVER -> Executor { block ->
-            val task = TickTask(Int.MAX_VALUE, block)
-            NeoForgeUtil.requireServer().doRunTask(task)
+            NeoForgeUtil.requireServer().executeBlocking(block) // .doRunTask(task)
         }
     }
 
