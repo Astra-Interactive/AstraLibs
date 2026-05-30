6bab70b520123f925873c1e8fa71244dc73164e0
Roman Makeev <makeevrserg@gmail.com>
2026-05-30 09:01:47 +0300

    [FIX] Add chunk ticket before teleporting

--- core-minecraft/src/main/kotlin/ru/astrainteractive/astralibs/server/util/TeleportableExt.kt
+++ core-minecraft/src/main/kotlin/ru/astrainteractive/astralibs/server/util/TeleportableExt.kt
@@ -1,6 +1,9 @@
 package ru.astrainteractive.astralibs.server.util
 
+import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.server.level.TicketType
+import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.storage.ServerLevelData
 import ru.astrainteractive.astralibs.server.Teleportable
 import ru.astrainteractive.klibs.mikro.core.util.tryCast
@@ -10,12 +13,20 @@ import ru.astrainteractive.klibs.mikro.core.util.tryCast
  */
 fun ServerPlayer.asTeleportable() = Teleportable { location ->
     val player = this
-    val level = MinecraftUtil.serverOrNull
-        ?.allLevels
-        ?.firstOrNull { serverLevel ->
+    val server = MinecraftUtil.serverOrNull ?: return@Teleportable
+    val level = server
+        .allLevels
+        .firstOrNull { serverLevel ->
             serverLevel.level.levelData.tryCast<ServerLevelData>()?.levelName == location.worldName
         }
         ?: return@Teleportable
+    val chunkPos = ChunkPos(BlockPos.containing(location.x, location.y, location.z))
+    level.chunkSource.addRegionTicket(
+        TicketType.POST_TELEPORT,
+        chunkPos,
+        server.playerList.simulationDistance,
+        player.id
+    )
     player.teleportTo(
         level,
         location.x,
